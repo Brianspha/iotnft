d5232c8e802dfd0e2cf587ef3a04eef93859b257
Siphamandla brian Mjoli <brianm445@yahoo.com>
2022-01-23 22:25:13 +0200

    update

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,12 +1,21 @@
-const IONFT = artifacts.require("IOTNFT");
-const IONFTToken = artifacts.require("TokenContract");
+var IONFT = artifacts.require("IOTNFT");
+var IONFTToken = artifacts.require("TokenContract");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(IONFTToken, "IOTNFT", "IOTNFT").then((IONFTTokenDeployed)=>{
+  deployer.deploy(IONFTToken, "IOTNFT", "IOTNFT").then((IONFTTokenDeployed) => {
     console.log("deployed token contract: ", IONFTTokenDeployed.address);
-   return deployer.deploy(IONFT, IONFTTokenDeployed.address).then(function(IONFTDeployed) {
-      console.log("deploying IONFT: ", IONFT.address);
-    });
+    return deployer
+      .deploy(IONFT, IONFTTokenDeployed.address)
+      .then(async function(IONFTDeployed) {
+        IONFT = await IONFT.deployed();
+        IONFTToken = await IONFTToken.deployed();
+        var receipt = await IONFTToken.setContractIOTNFTAddress(
+          IONFTDeployed.address
+        );
+        console.log(
+          "Succesfully set ionft address inside nft contract: ",
+          receipt
+        );
+      });
   });
- 
 };
